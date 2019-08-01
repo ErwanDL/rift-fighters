c52c90894ab1732c352d361c232b8d097c0c4025
Erwan De Lepinau <erwan.de-lepinau@student.ecp.fr>
2019-08-01 16:30:29 +0200

    implemented movement towards right

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -2,11 +2,16 @@
 
 public class CharacterMovement : MonoBehaviour {
 
-    private void Start () {
-
-    }
+    [SerializeField]
+    private float movementSpeed = 10f;
 
     void Update () {
-
+        Vector3 eulerRotation = transform.rotation.eulerAngles;
+        transform.rotation = Quaternion.Euler (eulerRotation.x, eulerRotation.y, 0);
+        if (Input.GetKey ("right")) {
+            Vector3 currentPos = transform.position;
+            float newX = currentPos.x + movementSpeed * Time.deltaTime;
+            transform.position = new Vector3 (newX, currentPos.y, currentPos.z);
+        }
     }
 }
