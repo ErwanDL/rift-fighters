da6947d0f5fdc13d20446c9fc04d7f1cd5278e3a
Erwan De Lepinau <erwan.de-lepinau@student.ecp.fr>
2019-08-02 13:00:53 +0200

    added yasuo model with run and idle animations

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 
-public class CharacterMovement : MonoBehaviour
-{
+public class CharacterMovement : MonoBehaviour {
 
     [SerializeField]
     private float acceleration = 15f;
@@ -15,47 +14,41 @@ public class CharacterMovement : MonoBehaviour
     [SerializeField]
     private Rigidbody2D rb;
 
-    private bool lastDir = true;    //true si droite, false si gauche
+    private bool lastDir = true; //true si droite, false si gauche
 
     private float fallMultiplier = 5.5f;
     private float lowMultiplier = 5.0f;
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
+    private void Start () {
+        rb = GetComponent<Rigidbody2D> ();
     }
     private float jumpVelocity = 50f;
-    void Update()
-    {
-        if (Input.GetKey("right"))
-        {
-            if (lastDir == false)
-            {
+    void Update () {
+        if (Input.GetKey ("right")) {
+            if (lastDir == false) {
                 Vector3 currentRot = transform.rotation.eulerAngles;
                 float rotY = currentRot.y + 180;
-                Vector3 newRot = new Vector3(currentRot.x, rotY, currentRot.z);
-                transform.rotation = Quaternion.Euler(newRot);
+                Vector3 newRot = new Vector3 (currentRot.x, rotY, currentRot.z);
+                transform.rotation = Quaternion.Euler (newRot);
             }
-            GetCurrentSpeed(lastKeyPressed == "right");
+            GetCurrentSpeed (lastKeyPressed == "right");
             Vector3 currentPos = transform.position;
             float newX = currentPos.x + currentSpeed * Time.deltaTime;
-            transform.position = new Vector3(newX, currentPos.y, currentPos.z);
+            transform.position = new Vector3 (newX, currentPos.y, currentPos.z);
             lastKeyPressed = "right";
             lastDir = true;
             return;
         }
-        if (Input.GetKey("left"))
-        {
-            if (lastDir == true)
-            {
+        if (Input.GetKey ("left")) {
+            if (lastDir == true) {
                 Vector3 currentRot = transform.rotation.eulerAngles;
                 float rotY = currentRot.y + 180;
-                Vector3 newRot = new Vector3(currentRot.x, rotY, currentRot.z);
-                transform.rotation = Quaternion.Euler(newRot);
+                Vector3 newRot = new Vector3 (currentRot.x, rotY, currentRot.z);
+                transform.rotation = Quaternion.Euler (newRot);
             }
-            GetCurrentSpeed(lastKeyPressed == "left");
+            GetCurrentSpeed (lastKeyPressed == "left");
             Vector3 currentPos = transform.position;
             float newX = currentPos.x - currentSpeed * Time.deltaTime;
-            transform.position = new Vector3(newX, currentPos.y, currentPos.z);
+            transform.position = new Vector3 (newX, currentPos.y, currentPos.z);
             lastKeyPressed = "left";
             lastDir = false;
             return;
@@ -63,32 +56,23 @@ public class CharacterMovement : MonoBehaviour
         lastKeyPressed = null;
 
         //JUMP
-        if (Input.GetKeyDown("space"))
-        {
+        if (Input.GetKeyDown ("space")) {
             rb.velocity = Vector2.up * jumpVelocity;
         }
 
-        if (rb.velocity.y < 0)
-        {
+        if (rb.velocity.y < 0) {
             rb.velocity += Vector2.up * Physics2D.gravity.y * fallMultiplier * Time.deltaTime;
-        }
-        else if (rb.velocity.y > 0 && !Input.GetKeyDown("space"))
-        {
+        } else if (rb.velocity.y > 0 && !Input.GetKeyDown ("space")) {
             rb.velocity += Vector2.up * Physics2D.gravity.y * lowMultiplier * Time.deltaTime;
         }
     }
-    void GetCurrentSpeed(bool isSameKey)
-    {
-        if (isSameKey)
-        {
+    void GetCurrentSpeed (bool isSameKey) {
+        if (isSameKey) {
             currentSpeed += acceleration * Time.deltaTime;
-        }
-        else
-        {
+        } else {
             currentSpeed = acceleration * Time.deltaTime;
         }
-        if (currentSpeed > maxSpeed)
-        {
+        if (currentSpeed > maxSpeed) {
             currentSpeed = maxSpeed;
         }
     }
