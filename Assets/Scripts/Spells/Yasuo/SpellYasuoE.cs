84632ba1ea81bc026f1c3e6a923263f66d6763d3
Ewifly <ferreol.delepinau@gmail.com>
2019-08-06 19:20:25 +0200

    Improved UI (Lifebars, PLayer Names...) + Yasuo's Dash (not functionnal) + cleaned up CharacterMovement

---
+++ Assets/Scripts/Spells/Yasuo/SpellYasuoE.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class SpellYasuoE : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public DashState dashState;
+    public float dashTimer;
+    public float maxDash = 20f;
+
+    public Vector2 savedVelocity;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        switch (dashState)
+        {
+            case DashState.Ready:
+                bool isDashKeyDown = Input.GetKeyDown("e");
+                if (isDashKeyDown)
+                {
+                    savedVelocity = rb.velocity;
+                    rb.velocity += new Vector2(rb.velocity.x * 3500f, rb.velocity.y);
+                    dashState = DashState.Dashing;
+                }
+                break;
+            case DashState.Dashing:
+                dashTimer += Time.deltaTime * 3;
+                if (dashTimer >= maxDash)
+                {
+                    dashTimer = maxDash;
+                    rb.velocity = savedVelocity;
+                    dashState = DashState.Cooldown;
+                }
+                break;
+            case DashState.Cooldown:
+                dashTimer -= Time.deltaTime;
+                if (dashTimer <= 0)
+                {
+                    dashTimer = 0;
+                    dashState = DashState.Ready;
+                }
+                break;
+        }
+    }
+}
+
+public enum DashState
+{
+    Ready,
+    Dashing,
+    Cooldown
+}
+
---
+++ Assets/Scripts/UIManager/DisplayName.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayName : MonoBehaviour
+{
+
+    [SerializeField, Tooltip("Drag & Drop a Champion")]
+    private GameObject character = null;
+
+    string champion = null;
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        champion = character.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (champion != null)
+        {
+            // Debug.Log(character.GetComponent<Player>().currentHealth);
+            text.text = " " + champion;
+        }
+    }
+
+}
