ca35b35ed8b6305c1b390de3354a2ad5c3d4a2a3
Erwan De Lepinau <erwan.de-lepinau@student.ecp.fr>
2019-08-07 15:57:27 +0200

    added auto attacking mechanic with damage dealing, cleaned up health bars UI and code

---
+++ Assets/Scripts/Spells and Attacks/Auto Attack/WeaponTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class WeaponTrigger : MonoBehaviour
+{
+    [System.Serializable]
+    private class MyEvent : UnityEvent<Collider2D> { }
+
+    [SerializeField]
+    private MyEvent triggerEvent = null;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == 8 || other.gameObject.layer == 9)
+            triggerEvent.Invoke(other);
+    }
+}
